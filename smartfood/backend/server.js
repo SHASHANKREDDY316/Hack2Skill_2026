298eb666b10b7ea1960c0a779fa6bf27bcd1ade2
SANKEPALLE SHASHANK REDDY <1da23cs153.cs@drait.edu.in>
2026-04-06 16:49:02 +0530

    Update server.js

---
+++ smartfood/backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("SmartFood Backend Running");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
